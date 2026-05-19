685be4db3745508762d5ac39bed29378bad61c31
haruki33 <kkhk0869hs33@gmail.com>
2026-05-19 11:34:53 +0900

    新規プロジェクトの作成

---
+++ src/main/java/com/examplew/GitSampleShimizuApplication.java
@@ -0,0 +1,13 @@
+package com.examplew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleShimizuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleShimizuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examplew/GitSampleShimizuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examplew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSampleShimizuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
